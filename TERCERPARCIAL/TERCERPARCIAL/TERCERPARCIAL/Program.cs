e6b130321d078138ca5777d09e23acea6b5a7ccf
brayan usme <cuentaparavisuall@outlook.com>
2024-11-29 20:41:12 -0500

    actua1.2

--- TERCERPARCIAL/TERCERPARCIAL/TERCERPARCIAL/Program.cs
+++ TERCERPARCIAL/TERCERPARCIAL/TERCERPARCIAL/Program.cs
@@ -6,7 +6,14 @@
         int n, m;
 
         //PEDIR AL USUARIO EL VALOR DE LAS DIMENSIONES
-        Console.WriteLine(" ingrese ");
+        Console.WriteLine(" Ingrese el número de filas (n):");
+        n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese el número de columnas (m):");
+        m = Convert.ToInt32(Console.ReadLine());
+
+        //declarar la matriz y hacer que tenga valores aleatorios
+
+        int[,] Matriz = new int[n, m];
 
     }
 
