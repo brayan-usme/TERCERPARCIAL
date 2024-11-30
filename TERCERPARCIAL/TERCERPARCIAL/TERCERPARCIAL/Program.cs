69d5fd00804c71f222dbfeec7044f0951af6f54e
brayan usme <cuentaparavisuall@outlook.com>
2024-11-29 20:36:28 -0500

    act1.1

--- TERCERPARCIAL/TERCERPARCIAL/TERCERPARCIAL/Program.cs
+++ TERCERPARCIAL/TERCERPARCIAL/TERCERPARCIAL/Program.cs
@@ -2,8 +2,11 @@
 {
     private static void Main(string[] args)
     {
+        
         int n, m;
 
+        //PEDIR AL USUARIO EL VALOR DE LAS DIMENSIONES
+        Console.WriteLine(" ingrese ");
 
     }
 
